3fca7547a8fd07ecf13de15d977766615c19c37e
studiobgd <125088274+studiobgd@users.noreply.github.com>
2023-02-12 11:28:59 +0200

    Update CurrencyFormatter.js

--- src/components/CurrencyFormatter/CurrencyFormatter.js
+++ src/components/CurrencyFormatter/CurrencyFormatter.js
@@ -4,7 +4,7 @@ import { isNumeric } from '../../helpers/general';
 
 const CurrencyFormatter = ({
   amount,
-  currency = 'USD',
+  currency = 'LEU',
   appendZero = false,
   useDollar = false,
 }) => {
